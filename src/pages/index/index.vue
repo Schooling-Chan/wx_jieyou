694063e6f7f0ef1f6c7841dd5614883579596f18
Schooling-Chan <55311886+Schooling-Chan@users.noreply.github.com>
2020-04-19 17:29:17 +0800

    完善首页1.0

---
+++ src/pages/index/index.vue
@@ -0,0 +1,360 @@
+<template>
+  <main class="mainBox">
+    <!-- 头部 -->
+    <div class="headBox">
+      <div class="judge">
+        <img src="../../../static/images/machine.png" alt="">
+        <div class="judge-text">
+          <div>智能问诊</div>
+          <span>答题预诊当前情况</span>
+        </div>
+      </div>
+      <div class="doctor">
+        <img src="../../../static/images/doctor.png" alt="">
+        <div class="doctor-text">
+          <div>咨询专家</div>
+          <span>精神科门诊专家</span>
+        </div>
+      </div>
+    </div>
+
+    <!-- 每日一记 -->
+    <div class="noteBox">
+      <div class="title"><div class="title-text">打卡</div></div>
+      <div class="content">
+        <div class="medical">
+          <div class="comment-pic"><img src="../../../static/images/kitmedical.png" alt=""></div>
+          <span>用药记录</span>
+        </div>
+        <div class="weight">
+          <div class="comment-pic"><img src="../../../static/images/weight.png" alt=""></div>
+          <span>体重记录</span>
+        </div>
+        <div class="hospital">
+          <div class="comment-pic"><img src="../../../static/images/hospital.png" alt=""></div>
+          <span>附近医院</span>
+        </div>
+      </div>
+    </div>
+
+    <!-- 热门帖子 -->
+    <div class="invitationBox">
+      <div class="title"><div class="title-text">热门动态</div></div>
+      <div class="invitation">
+        <div class="invitation-persons">
+          <div class="invitation-persons-head">
+            <img src="../../../static/images/1.jpg" alt="头像" class="perImg">
+            <span>心猿意马</span>
+            <div class="time">2020-01-02 18:00:00 </div>
+            <!-- <div class="iconfont icong">1110</div> -->
+          </div>
+          <div class="invitation-persons-content">
+            <div class="invitation-persons-content-title">C/C++语言学习交流qun，从基础到项目实战，全方面系统分享 </div>
+            <div class="details">目前信息化产业发展势头很好，互联网就成为了很多普通人想要涉及的行业，因为相比于传统行业，互联网行业涨薪幅度大，机会也多，所以就会大批的人想要转行来学习c++开发。
+            目前来讲市场上需要的c++人员非常多，而且按照现在的势头，以后会需要更多的c++开发人员，理由是以后每个人公司都会有自己的网站，有自己的开发部，对于用户体验看的非常重要。所以c++程序员就会很吃香。
+            学习c++语言，除了要学习语法，还要学习内存、字符编码、调试技巧以及编程思维，这些知识点本教程都做了详细讲解。
+            加入我的C/C++语言学习交流qq裙：110355025，免费领取，整理了一套最新的C语言基础教程。这套「c语言入门教程将多年的编程经验灌输其中，典型的实践派。既适合初学者入门（学习语法），也适合程序员进阶（学习底层）。
+            </div>
+          </div>
+          <div class="footer">
+            <div class="give iconfont icond">55</div>
+            <div class="comment iconfont iconc">55</div>
+          </div>
+        </div>
+        <div class="invitation-persons">
+          <div class="invitation-persons-head">
+            <img src="../../../static/images/1.jpg" alt="头像" class="perImg">
+            <span>心猿意马</span>
+            <div class="time">2020-01-02 18:00:00</div>
+          </div>
+          <div class="invitation-persons-content">
+            <div class="invitation-persons-content-title">C/C++语言学习交流qun，从基础到项目实战，全方面系统分享 </div>
+            <div class="details">目前信息化产业发展势头很好，互联网就成为了很多普通人想要涉及的行业，因为相比于传统行业
+            </div>
+          </div>
+          <div class="footer">
+            <div class="give iconfont icond">55</div>
+            <div class="comment iconfont iconc">55</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+
+
+export default {
+  
+}
+</script>
+
+<style scoped lang="scss">
+@import url('../../../static/fonts/invitation/index.css');
+$bg:#f0f0f0;
+.mainBox{
+  background: $bg;
+  font-size: 100rpx;
+  overflow-x: hidden;
+}
+//公共题目
+.title{
+    height: 1rem;
+    font-size: .36rem;
+    font-weight: 666;
+    margin-top: 10rpx;
+    line-height: 1rem;
+    background: $bg;
+    position: relative;
+    color: rgb(151, 151, 151);
+    &-text{
+      width: 28%;
+      text-align: center;
+      background: $bg;
+      position: absolute;
+      left: 0;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      margin: auto;
+
+    }
+
+    &::before{
+      content: '';
+      display: block;
+      position: absolute;
+      left: 0;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      margin: auto;
+      width: 34%;
+      height: 4rpx;
+      background: #ccc;
+
+    }
+    
+}
+// headBox
+.headBox{
+  // margin: .3rem auto;
+  width: 100%;
+  background: #fff;
+  height: 2.6rem;
+  line-height: 2.6rem;
+  display: flex;
+  flex-wrap: nowrap;
+  overflow: hidden;
+  box-sizing: border-box;
+  align-items: center;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,.1);
+  .judge,.doctor{
+    background: linear-gradient(45deg, rgb(26,222,224), rgb(230, 230, 230));
+    flex: 1;
+    width: 35%;
+    line-height: 2.08rem;
+    margin: 0 .5rem;
+    height: 2.08rem;
+    border-radius: 25rpx;
+    overflow: hidden;
+    position: relative;
+    img{
+      width: .6rem;
+      height: 0.6rem;
+      position: absolute;
+      top: 10%;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    &-text{
+      width: 100%;
+      text-align: center;
+      display: flex;
+      flex-direction: column;
+      color: #fff;
+      height: 100%;
+      overflow: hidden;
+      div{
+        width: 100%;
+        font-size: .36rem;
+        font-weight: 666;
+        height: 0.5rem;
+        line-height: .5rem;
+        overflow: hidden; 
+        margin-top: .88rem;
+      }
+      span{
+        font-size: .20rem;
+        display: inline-block;
+        height: 0.25rem;
+        line-height: 0.25rem;
+      }
+
+      &:hover{
+        background: rgba($color: #fff, $alpha: .5);
+      }
+    }
+  }
+  .doctor{
+    background: linear-gradient(45deg, rgb(96,160,250), rgb(230, 230, 230));
+  }
+}
+// noteBox
+.noteBox{
+  position: relative;
+  min-height: 3rem;
+  font-size: .26rem;
+  // margin: 0 auto .3rem auto;
+  
+
+  .medical .comment-pic{
+    background: linear-gradient(45deg, rgb(191,213,252), rgb(230, 230, 230));
+  }
+  .hospital .comment-pic{
+    background: linear-gradient(45deg, rgb(156,222,255), rgb(230, 230, 230));
+  }
+  .weight .comment-pic{
+    background: linear-gradient(45deg, rgb(140,229,227), rgb(230, 230, 230));
+  }
+
+  .content{
+    background: #fff;
+    height: 1.9rem;
+    line-height: 1.9rem;
+    display: flex;
+    overflow: hidden;
+
+    .comment-pic{
+      position: relative;
+      height: 1rem;
+      width: 1rem;
+      line-height: 1rem;
+      border-radius: 50%;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      img{
+        height: 50%;
+        width: 50%;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+      }
+      &:hover{
+        background: rgba($color: rgb(151, 151, 151), $alpha: .5);
+      }
+    }
+
+    .medical, .hospital,.weight{
+      height: 1.6rem;
+      text-align: center;
+      flex: 2;
+      display: flex;    
+      flex-direction: column;
+      justify-content:space-around;
+      overflow: hidden;
+      margin: 0 auto;
+
+      span{
+        height: .3rem;
+        line-height: .3rem;
+        width: 100%;
+        margin-top: .39rem;
+        display: block;
+        color: rgb(151, 151, 151);
+      }
+    }
+    
+  }
+}
+
+//invitationBox
+.invitationBox{
+  .invitation{
+    
+    min-height: 2.5rem;
+    &-persons{
+      min-height: 3.5rem;
+      margin-bottom: .15rem;
+      background: #fff;
+      border-radius: .15rem;
+      box-shadow: 0 2px 4px 0 rgba(0,0,0,.1);
+      &-head{
+        width: 100%;
+        height: 1rem;
+        font-size: .26rem;
+        position: relative;
+        border-bottom: 1px solid rgb(237,238,242);
+        
+        .perImg{
+          width: 0.6rem;
+          height: 0.6rem;
+          border-radius: 50%;
+          position: absolute;
+          top: 50%;
+          left: 3%;
+          transform: translateY(-50%);
+        }
+        span{
+          position: absolute;
+          font-weight: 666;
+          left: 15%;
+          top: 18%;
+          color: rgba($color: #000, $alpha: .8);
+        }
+        .time{
+          display: inline;
+          position: absolute;
+          bottom: 5%;
+          left: 18%;
+          color: rgba($color: #000, $alpha: .5);
+        }
+      }
+
+      &-content{
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        // text-align: center;
+        padding: .2rem .2rem 0 .2rem;
+        min-height: 2.5rem;
+        &-title{
+          font-size: .35rem;
+          font-weight: 555;
+          overflow-y: hidden;
+        }
+        .details{
+          font-size: .3rem;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: normal;
+          display: -webkit-box;
+          word-break: break-word;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 3; 
+          margin-top: .2rem;
+          color: rgba($color: #000, $alpha: .7);
+        }
+        
+      }
+      .footer{
+          height: 0.5rem;
+          padding: 0 .2rem;
+          position: relative;
+          font-size: .3rem;
+          .comment, .give{
+            position: absolute;
+            right: 5%;
+            top: -10%;
+          }
+          .give{
+            right: 21%;
+          }
+      }
+    }
+  }
+}
+</style>
