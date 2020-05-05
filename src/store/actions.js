81ed8e37c6b7561d60e731a71115465389b61e30
Schooling-Chan <55311886+Schooling-Chan@users.noreply.github.com>
2020-05-05 23:44:09 +0800

    合并更新1.0.11

--- src/store/actions.js
+++
@@ -1,8 +0,0 @@
-import {RECEIVE_USER_INFO} from './mutation-type'
-
-export default {
-  getUuserInfo({commit},{userInfo}){
-    // 触发对应的mutation
-    commit(RECEIVE_USER_INFO, userInfo)
-  }
-}
--- src/store/state.js
+++
@@ -1,3 +0,0 @@
-export default {
-  userInfo:[]//个人信息
-}
